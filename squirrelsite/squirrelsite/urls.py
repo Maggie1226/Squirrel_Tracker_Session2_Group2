8bbaffd37d16206a090ff0017c888e66ad78cdb8
Ming Jin <mj2940@columbia.edu>
2019-12-06 13:55:33 -0500

    Build Map

--- squirrelsite/squirrelsite/urls.py
+++ squirrelsite/squirrelsite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns =[ 
+        path('',include('map.urls')),
         path('sightings/',include('sightings.urls')),
         path('admin/', admin.site.urls),
 ]
