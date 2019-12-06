85e3475b05e65f9e5bced55e30f019b3e6659e4b
Alice Ma <yixuanma17@gmail.com>
2019-12-06 19:21:24 +0000

    new update

--- squirrelsite/sightings/management/commands/export_squrriel_data.py
+++ squirrelsite/sightings/management/commands/export_squrriel_data.py
@@ -1,18 +1,15 @@
 from django.core.management.base import BaseCommand
-from .models import squirrel_site
+from sightings .models import squirrel_site
 import csv
 
 class Command(BaseCommand):
     def add_arguments(self,parser):
-        parser.add_arguments("csv_file")
-
-    def get_model_fields(model):
-        return model._meta.fields
+        parser.add_argument("csv_file")
 
     def handle(self, *args, **options):
         with open(options['csv_file'],"w",newline="") as fp:
             writer=csv.writer(fp)
-            field_names=[f.name for f in get_model_fields(squirrel_site)]
+            field_names=[f.name for f in squirrel_site._meta.fields]
             writer.writerow(field_names)
             for obj in squirrel_site.objects.all():
                 row=""
