d920c94bf5ec2101c0c0196dadcb3a0c9b736909
Ming Jin <mj2940@columbia.edu>
2019-12-06 23:59:50 -0500

    modify url

--- squirrelsite/sightings/urls.py
+++ squirrelsite/sightings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
         path('',views.all_squirrels),
-        re_path(r'(\d{2}[a-zA-Z].[a-zA-Z]{2}.\d{4}.\d{2})/',views.edit_squirrel),
+        re_path(r'(\d.*)/',views.edit_squirrel),
         path('add/',views.add),
         path('stats/',views.stats),
         ]
