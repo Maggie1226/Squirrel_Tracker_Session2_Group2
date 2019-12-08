b7171119829a33bc90e4f784c109628dcb6bffbd
Ming Jin <mj2940@columbia.edu>
2019-12-07 21:10:33 -0500

    move env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mj2940/Squirrel_Tracker/Squirrel_Tracker_Session2_Group2/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
